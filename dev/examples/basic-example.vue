1d808ff7e8a2c9931b7b5dc8ae2e59df8610e7d2
Muhi Masri <Muhi.Masri@mdacorporation.com>
2021-06-21 21:01:13 -0400

    Basic example

---
+++ dev/examples/basic-example.vue
@@ -0,0 +1,52 @@
+<template>
+    <b-editable-table :items="items" :fields="fields" @input-change="handleInput"></b-editable-table>
+</template>
+
+<script>
+import BEditableTable from '@/b-editable-table.vue';
+export default {
+  components: {
+    BEditableTable
+  },
+  data() {
+    return {
+      fields: [
+        { key: "name", label: "Name", type: "text"},
+        { key: "department", label: "Department", type: "select", options: ['Marketing', 'Development', 'HR'] },
+        { key: "age", label: "Age", type: "number" },
+        { key: "dateOfBirth", label: "Date Of Birth", type: "date" },
+        { key: "isActive", label: "Is Active", type: "check", formatter: (value, key, item) => {
+              return value ? 'Yes' : 'No'
+            }, },
+      ],
+       items: [
+          { age: 40, name: 'Dickerson', department: 'Development', dateOfBirth: '1984-05-20', isActive: true },
+          { age: 21, name: 'Larsen', department: 'Marketing', dateOfBirth: '1984-05-20', isActive: false },
+          { age: 89, name: 'Geneva', department: 'HR', dateOfBirth: '1984-05-20', isActive: false },
+          { age: 38, name: 'Jami', department: 'Accounting', dateOfBirth: '1984-05-20', isActive: true }
+        ]
+    };
+  },
+  methods: {
+      handleInput(value, data) {
+          console.log(value);
+          console.log(data);
+      }
+  }
+};
+</script>
+
+<style>
+.b-table input {
+
+}
+thead, tbody, tfoot, tr, td, th {
+  text-align: left;
+  width: 100px;
+  vertical-align: middle;
+}
+pre {
+  text-align: left;
+  color: #d63384;
+}
+</style>
--- dev/serve.vue
+++ dev/serve.vue
@@ -1,50 +1,22 @@
 <script lang="ts">
 import Vue from 'vue';
-import BEditableTable from '@/b-editable-table.vue';
-import BExample from './b-example.vue';
+import CompleteExample from './examples/complete-example.vue';
+import BasicExample from './examples/basic-example.vue';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default Vue.extend({
   name: 'ServeDev',
   components: {
-    BEditableTable,
-    BExample
-  },
-  data() {
-    return {
-      fields: [
-        { key: "name", label: "Name", type: "text"},
-        { key: "department", label: "Department", type: "select", options: ['Marketing', 'Development', 'HR'] },
-        { key: "age", label: "Age", type: "number" },
-        { key: "dateOfBirth", label: "Date Of Birth", type: "date" },
-        { key: "permanentResident", label: "Permanent Resident", type: "check" },
-      ],
-       items: [
-          { age: 40, name: 'Dickerson', department: 'Development', dateOfBirth: '1984-05-20', permanentResident: true },
-          { age: 21, name: 'Larsen', department: 'Marketing', dateOfBirth: '1984-05-20', permanentResident: false },
-          { age: 89, name: 'Geneva', department: 'HR', dateOfBirth: '1984-05-20', permanentResident: false },
-          { age: 38, name: 'Jami', department: 'Accounting', dateOfBirth: '1984-05-20', permanentResident: true }
-        ]
-    };
-  },
-  methods: {
-      handleInput(value: any, data: any) {
-          console.log(value);
-          console.log(data);
-      }
+    CompleteExample,
+    BasicExample
   }
 });
 </script>
 
 <template>
   <div id="app">
-    <!-- <b-editable-table :items="items" :fields="fields" @input-change="inputHandler">
-      <template #cell-permanentResident="data">
-        <span v-if="data.value">Yes</span>
-        <span v-else>No</span>
-      </template>
-    </b-editable-table> -->
-    <b-example></b-example>
+    <basic-example></basic-example>
+    <complete-example></complete-example>
   </div>
 </template>
