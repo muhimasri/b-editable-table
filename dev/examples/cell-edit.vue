4b65fd2d0b623cbff9d822f6331b06ef5660f31b
Muhi Masri <muhi@muhimasri.com>
2023-12-14 07:14:23 -0500

    Add scoped and prettier

--- dev/examples/cell-edit.vue
+++ dev/examples/cell-edit.vue
@@ -19,8 +19,8 @@
 </template>
 
 <script>
-import BEditableTable from '@/b-editable-table.vue'
-import { BButton } from 'bootstrap-vue'
+import BEditableTable from "@/b-editable-table.vue";
+import { BButton } from "bootstrap-vue";
 export default {
   components: {
     BEditableTable,
@@ -29,106 +29,108 @@ export default {
     return {
       fields: [
         {
-          key: 'name',
-          label: 'Name',
-          type: 'text',
+          key: "name",
+          label: "Name",
+          type: "text",
           editable: true,
-          placeholder: 'Enter Name...',
-          class: 'name-col',
+          placeholder: "Enter Name...",
+          class: "name-col",
         },
         {
-          key: 'department',
-          label: 'Department',
-          type: 'select',
+          key: "department",
+          label: "Department",
+          type: "select",
           editable: true,
-          class: 'department-col',
+          class: "department-col",
           options: [
-            { value: 1, text: 'HR' },
-            { value: 2, text: 'Engineer' },
-            { value: 3, text: 'VP' },
-            { value: 4, text: 'CEO' },
+            { value: 1, text: "HR" },
+            { value: 2, text: "Engineer" },
+            { value: 3, text: "VP" },
+            { value: 4, text: "CEO" },
           ],
         },
         {
-          key: 'age',
-          label: 'Age',
-          type: 'range',
-          min: '0',
-          max: '100',
+          key: "age",
+          label: "Age",
+          type: "range",
+          min: "0",
+          max: "100",
           editable: true,
-          placeholder: 'Enter Age...',
-          class: 'age-col',
+          placeholder: "Enter Age...",
+          class: "age-col",
         },
         {
-          key: 'dateOfBirth',
-          label: 'Date Of Birth',
-          type: 'date',
+          key: "dateOfBirth",
+          label: "Date Of Birth",
+          type: "date",
           editable: true,
-          class: 'date-col',
-          locale: 'en',
-          'date-format-options': {
-            year: 'numeric',
-            month: 'numeric',
-            day: 'numeric',
+          class: "date-col",
+          locale: "en",
+          "date-format-options": {
+            year: "numeric",
+            month: "numeric",
+            day: "numeric",
           },
         },
         {
-          key: 'isActive',
-          label: 'Is Active',
-          type: 'checkbox',
+          key: "isActive",
+          label: "Is Active",
+          type: "checkbox",
           editable: true,
-          class: 'is-active-col',
+          class: "is-active-col",
         },
       ],
       items: [
         {
           id: 1,
           age: 40,
-          name: 'Dickerson',
+          name: "Dickerson",
           department: 1,
-          dateOfBirth: '1984-05-20',
+          dateOfBirth: "1984-05-20",
           isActive: true,
         },
         {
           id: 2,
           age: 21,
-          name: 'Larsen',
+          name: "Larsen",
           department: 2,
-          dateOfBirth: '1972-07-25',
+          dateOfBirth: "1972-07-25",
           isActive: false,
         },
         {
           id: 3,
           age: 89,
-          name: 'Geneva',
+          name: "Geneva",
           department: 3,
-          dateOfBirth: '1981-02-02',
+          dateOfBirth: "1981-02-02",
           isActive: false,
         },
         {
           id: 4,
           age: 38,
-          name: 'Jami',
+          name: "Jami",
           department: 4,
-          dateOfBirth: '1964-10-19',
+          dateOfBirth: "1964-10-19",
           isActive: true,
         },
       ],
-    }
+    };
   },
   methods: {
     handleInput(data) {},
   },
-}
+};
 </script>
 
 <style>
 table.editable-table {
   margin: auto;
+  width: auto;
 }
 
 table.editable-table td {
   vertical-align: middle;
+  padding: 0;
 }
 
 .editable-table .data-cell {
--- dev/serve.vue
+++ dev/serve.vue
@@ -1,37 +1,37 @@
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 // import InputValidation from './examples/input-validation.vue'
-import CompleteExample from './examples/complete-example.vue';
-// import CellEdit from './examples/cell-edit.vue'
+// import CompleteExample from './examples/complete-example.vue';
+import CellEdit from "./examples/cell-edit.vue";
 // import RowEdit from './examples/row-edit.vue';
 // import LoadApi from './examples/load-api.vue';
 // import OneWayBinding from './examples/one-way-binding.vue';
 // import AddRemoveUpdate from './examples/add-remove-update.vue';
 // import Paging from './examples/paging.vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 export default Vue.extend({
-  name: 'ServeDev',
+  name: "ServeDev",
   components: {
-    CompleteExample,
+    // CompleteExample,
     // InputValidation,
     // RowEdit,
     // LoadApi,
     // OneWayBinding,
     // AddRemoveUpdate,
-    // CellEdit,
+    CellEdit,
     // Paging,
   },
-})
+});
 </script>
 
 <template>
   <div id="app">
     <!-- <input-validation></input-validation> -->
-    <!-- <cell-edit></cell-edit> -->
+    <cell-edit></cell-edit>
     <!-- <row-edit></row-edit> -->
-    <complete-example></complete-example>
+    <!-- <complete-example></complete-example> -->
     <!-- <load-api></load-api> -->
     <!-- <one-way-binding></one-way-binding> -->
     <!-- <add-remove-update></add-remove-update> -->
